971531c79a626d9b6f7e67bdc99e6d990995bbc5
Dikshit <singhdikshit69@gmail.com>
2023-02-12 18:29:33 +0530

    Add new repo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 const qrcode = require("qrcode-terminal");
 var profanity = require("profanity-hindi");
-const { Client, LocalAuth } = require("whatsapp-web.js");
+const {Client, LocalAuth,MessageMedia } = require("whatsapp-web.js");
 const { Configuration, OpenAIApi } = require("openai");
 
 dotenv.config();
@@ -27,13 +27,14 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+
 //& for normal searches or find the answer of any general query
 async function searchNotes(topic) {
   const chatResponse = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: topic,
     temperature: 0.3,
-    max_tokens: 1024,
+    max_tokens: 2046,
     top_p: 1.0,
     frequency_penalty: 0.0,
     presence_penalty: 0.6,
@@ -58,12 +59,12 @@ async function codex(text) {
 
 // Listen for incoming messages from users
 client.on("message", async (msg) => {
-  //Check for profanity words
-  var isDirty = profanity.isMessageDirty(msg.body);
-  if (isDirty) {
-    msg.reply("Warning! Don't Abuse Here\n");
-  }
-//   await client.sendMessage(msg.from, "undefined");
+//   //Check for profanity words
+//   var isDirty = profanity.isMessageDirty(msg.body);
+//   if (isDirty) {
+//     msg.reply("Warning! Don't Abuse Here\n");
+  // }
+  // await client.sendMessage(msg.from, "Hey there! I'm a bot and my boss will be available after 11 pm tonight. So keep ur messages to urself until 11😁");
 
   if (msg.body == "menu" || msg.body == "Menu") {
     await client.sendMessage(
@@ -79,12 +80,23 @@ client.on("message", async (msg) => {
     const text = msg.body.split(", ")[0].split(": ")[1];
     console.log(text);
     let result = await searchNotes(text);
-    client.sendMessage(msg.from, `Ans:${result}`);
+    client.sendMessage(msg.from, `${result}`);
   } else if (msg.body.startsWith("c: ") || msg.body.startsWith("C: ")) {
     const text = msg.body.split(", ")[0].split(": ")[1];
     console.log(text);
     let result = await codex(text);
     client.sendMessage(msg.from, `${result}`);
+  }else if(msg.body.startsWith("i: ")|| msg.body.startsWith("I: ")){
+    const media = new MessageMedia.fromUrl('https://via.placeholder.com/350x150.png');
+    client.sendMessage(message.from,message.reply(await media));
   }
 });
+// client.getCountryCode(data=>console.log(data))
 client.initialize();
+// client.sendAudio(
+//   '+918860439648',
+//   '/audio.wav',
+//   {
+//     caption: 'This is an audio message'
+//   }
+// )
